a51659233d00ded6efcb2ba0d05976a8c850a0ef
hyeyeonkimm <githubkimhye0918@gmail.com>
2025-08-07 10:22:31 +0900

    수정7

--- js/event.js
+++ js/event.js
@@ -3,3 +3,9 @@ let btn1 = document.querySelector("#btn1");
 btn1.addEventListener("click", () =>{
     console.log("버튼1이 클릭됨.");
 });
+
+// 함수선언
+function greet(name){
+    return 'Hello, ${name}!';
+}
+console.log(Feet("Hyeyeon")); // 콘솔을 통해 호출
